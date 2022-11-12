7f89a49f3f7d4ee9ca11b1ab5a6af23907abc0a9
victorv47 <victor.vela94@gmail.com>
2022-11-12 17:20:02 -0600

    Added more robust configuration

---
+++ src/helpers/errors.js
@@ -0,0 +1,20 @@
+import logger from './logger'
+
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.orinalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+export const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  logger.error({
+    message: error.message,
+    stack: error.stack,
+  })
+  res.status(statusCode)
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '💩' : error.stack,
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+import './helpers/dotenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import logger from './helpers/logger'
+import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
-const port = 3000
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
 
-app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There, my name is Victor!' })
-})
+app.use(router)
 
-app.listen(port)
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+  logger.info(`Application started at http://localhost:${process.env.PORT}`),
+)
