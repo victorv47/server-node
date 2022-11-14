54d78b1420aa411872654bd02a386a61e0edb25f
victorv47 <victor.vela94@gmail.com>
2022-11-13 21:45:30 -0600

    Added missing returns

--- src/models/persons.js
+++ src/models/persons.js
@@ -11,6 +11,7 @@ export const getPerson = (id) => {
 export const createPerson = (person) => {
   const id = uuid()
   persons.push({ id, ...person })
+  return getPerson(id)
 }
 
 export const updatePerson = (id, person) => {
@@ -19,11 +20,14 @@ export const updatePerson = (id, person) => {
     const personIndex = persons.findIndex((p) => p.id === id)
     persons[personIndex] = { id, ...person }
   }
+  return getPerson(id)
 }
 
 export const deletePerson = (id) => {
   const personIndex = persons.findIndex((p) => p.id === id)
   if (personIndex !== -1) {
     persons.splice(personIndex, 1)
+    return true
   }
+  return false
 }
--- src/routes/api/persons.js
+++ src/routes/api/persons.js
@@ -17,22 +17,34 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const person = getPerson(req.params.id)
-  res.send(person)
+  if (person) {
+    res.send(person)
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
 router.post('/', (req, res) => {
   const newPerson = createPerson(req.body)
-  res.send(newPerson)
+  if (newPerson) {
+    res.status(201).send(newPerson)
+  }
+  res.status(404).send({ msg: 'Bad Request' })
 })
 
 router.put('/:id', (req, res) => {
-  updatePerson(req.params.id, req.body)
-  res.send({ msg: ` Person ${req.params.id} Updated` })
+  const updatedPerson = updatePerson(req.params.id, req.body)
+  if (updatedPerson) {
+    res.send(updatedPerson)
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
 router.delete('/:id', (req, res) => {
-  deletePerson(req.params.id, req.body)
-  res.send({ msg: `Person ${req.params.id} Deleted` })
+  const deleted = deletePerson(req.params.id, req.body)
+  if (deleted) {
+    res.send({ msg: `Person ${req.params.id} Deleted` })
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
 export default router
