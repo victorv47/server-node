93249afedea52df1109ef36aa20c3a5d9e89f423
victorv47 <victor.vela94@gmail.com>
2022-11-27 20:56:52 -0600

    Updated Department Call

--- src/models/departments.js
+++ src/models/departments.js
@@ -3,7 +3,7 @@ import db from '../utils/db'
 export const getDepartments = async () => db.department.findMany()
 
 export const getDepartment = async (id) =>
-  db.department.findUnique({ where: { DepartmentId: id } }) // Had to change to capital "D" cause getting "Unknown arg `departmentId` in where.departmentId for type DepartmentWhereUniqueInput"
+  db.department.findUnique({ where: { departmentId: id } }) // Had to change to capital "D" cause getting "Unknown arg `departmentId` in where.departmentId for type DepartmentWhereUniqueInput"
 
 export const addDepartment = async (name, companyId) =>
   db.department.create({ data: { name, companyId } })
